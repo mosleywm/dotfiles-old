34b64de68975d1f2caabe398fd63f2de79bb2014
William Mosley <mosleywm@gmail.com>
2016-02-23 09:30:45 -0800

    initial personal configuration

--- brew.sh
+++ brew.sh
@@ -3,10 +3,10 @@
 # Install command-line tools using Homebrew.
 
 # Ask for the administrator password upfront.
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -15,8 +15,8 @@ brew update
 brew upgrade --all
 
 # Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
+echo "Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`."
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
@@ -38,68 +38,54 @@ brew install wget --with-iri
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+#brew install ringojs
+#brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
+brew install homebrew/php/php55 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
+brew install node
+#brew install imagemagick --with-webp
+#brew install lua
+#brew install lynx
+#brew install p7zip
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rhino
+#brew install speedtest_cli
+#brew install ssh-copy-id
 brew install tree
 brew install webkit2png
-brew install zopfli
+#brew install zopfli
+
+# Install native apps
+brew install caskroom/cask/brew-cask
+
+brew tap caskroom/versions
+brew cask install alfred
+brew cask install atom
+brew cask install google-chrome
+brew cask install charles
+brew cask install dash
+brew cask install encryptr
+brew cask install evernote
+brew cask install firefox
+brew cask install iterm2
+brew cask install jumpcut
+brew cask install slack
+brew cask install spectacle
 
 # Remove outdated versions from the cellar.
 brew cleanup
